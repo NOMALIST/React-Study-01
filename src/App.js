eb6ef1ab1906d6ddcdb8d2c2c201b617ca97d690
nomalist <yojun926@naver.com>
2024-01-12 02:39:02 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PostingCard from './components/posting/PostingCard';
+import './App.css';
+import {data} from './data'
+
+function App() {
+ 
+  // const post = {postid:1 , postcontent:'abcd'};
+ 
+  return (
+    <>
+    {data.map((posting) => { 
+      return <PostingCard key={posting.postingId} posting={posting}/>;
+    })}
+
+    </>
+  );
+}
+
+export default App;
