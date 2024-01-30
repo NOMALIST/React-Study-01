09bd7bfb14b123eb9e5cc66086b06a38dd59ffc3
nomalist <yojun926@naver.com>
2024-01-30 21:38:26 +0900

    스켈레톤 UI 추가

--- src/pages/main/BlogMain.jsx
+++ src/pages/main/BlogMain.jsx
@@ -1,20 +1,28 @@
 
 import {useState, useEffect} from 'react';
 import PostingCard from '../posting/PostingCard';
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
 import axios from 'axios';
 
 
 const BlogMain = (props) => {
 
     const [apidata, setApidata] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     function addCardInfo() {
+
+        setIsLoading(true);
+
         axios.get('http://localhost:4000/homePostings')    
         .then((response)=>{
                 let newData = [...apidata, ...response.data];
                 setApidata(newData);
             })
         .catch((Error)=>{console.log(Error)})
+        
+        setIsLoading(false);
     }
 
     const handleScroll = () => {
@@ -23,13 +31,13 @@ const BlogMain = (props) => {
         let windowHeight = window.innerHeight; // 화면 크기
         let fullHeight = document.body.scrollHeight; // 전체 스크롤
 
-        if(scrollLocation + windowHeight + 1 >= fullHeight) {
+        if(scrollLocation + windowHeight + 2 >= fullHeight) {
             console.log('카드 추가');
             addCardInfo();
         }
-        // console.log('scrollLocation : ' + scrollLocation);
-        // console.log('windowHeight : ' + windowHeight);
-        // console.log('fullHeight :  ' + fullHeight);
+        console.log('scrollLocation : ' + scrollLocation);
+        console.log('windowHeight : ' + windowHeight);
+        console.log('fullHeight :  ' + fullHeight);
 
      };
 
@@ -38,19 +46,36 @@ const BlogMain = (props) => {
         .then((response)=>{
                 setApidata(response.data);
             })
-        .catch((Error)=>{console.log(Error)})
+        .catch((Error)=>{console.log(Error)});
+
         window.addEventListener("scroll", handleScroll);
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
     }, [])
        
-  
+    const SkeletonCard = () => {
+        return (
+            <Stack spacing={3}>
+              {/* For variant="text", adjust the height via font-size */}
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+              {/* For other variants, adjust the size with `width` and `height` */}
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="rectangular" width={210} height={60} />
+              <Skeleton variant="rounded" width={210} height={60} />
+            </Stack>
+          );
+    }
+
     return (
-        apidata.map((posting) => { 
-            return !!posting && <PostingCard key={posting.postingId} posting={posting}/>;
-        })
+        <div className='posting-container'>
+            <div className='postcard-wrapper inner-container'>
+                {!!apidata && apidata.map((posting, index) => {return <PostingCard key={index} posting={posting}/>; })}
+                {isLoading && <SkeletonCard/>}
+            </div>
+        </div>
     )
+
 
     
 }
